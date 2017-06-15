49873ae90631b2e674520602db89dc6c2e9577d6
Heidi Kasemir <heidi.kasemir@gmail.com>
2017-06-14 21:40:04 -0700

    fix per review comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,37 +1,39 @@
 import media
 import fresh_tomatoes
 
+# a list of my favorite Movies instantiated using the Movie class from media
 movies_list = [
         media.Movie(
             'Beauty and the Beast',
             'https://youtu.be/tRlzmyveDHE',
-            'http://static.tvtropes.org/pmwiki/pub/images/beauty_beast_poster.jpg'
+            'http://bit.ly/2s3yfMr'
         ),
         media.Movie(
             'Monty Python and the Holy Grail',
             'https://youtu.be/urRkGvhXc8w',
-            'http://gravillis.s3.amazonaws.com/wp-content/uploads/2013/11/Monty-Python-Holy-Grail-poster.jpg'
+            'http://bit.ly/2sqUqfY'
         ),
         media.Movie(
             'Pirates of the Caribbean: the Curse of the Black Pearl',
             'https://youtu.be/naQr0uTrH_s',
-            'https://www.movieposter.com/posters/archive/main/11/MPW-5924'
+            'http://bit.ly/2toMlFR'
         ),
         media.Movie(
             'The Jacket',
             'https://youtu.be/rCxQ83Pg1Ko',
-            'http://ecx.images-amazon.com/images/I/51Fx5qbu36L._SX940_.jpg'
+            'http://bit.ly/2toC78D'
         ),
         media.Movie(
             '10 Things I Hate About You',
             'https://youtu.be/vpVZS5nTxh8',
-            'https://ameliatcreativemedia.files.wordpress.com/2014/01/10thingsihateaboutyou.jpg'
+            'http://bit.ly/2toQ729'
         ),
         media.Movie(
             'Star Wars: The Force Awakens',
             'https://youtu.be/sGbxmsDFVnE',
-            'https://originalvintagemovieposters.com/wp-content/uploads/2016/05/Force-Awakens-4900-Large-705x1024.jpg'
+            'http://bit.ly/2rtIWZT'
         ),
     ]
 
 fresh_tomatoes.open_movies_page(movies_list)
+
--- media.py
+++ media.py
@@ -1,8 +1,11 @@
 import fresh_tomatoes
 
+
 class Movie():
-    '''Class for basic movie information, including title, trailer, and box art'''
+    '''Class for basic movie information,
+    including title, trailer, and box art'''
     def __init__(self, title, youtube_url, image_url):
         self.title = title
         self.trailer_youtube_url = youtube_url
         self.poster_image_url = image_url
+
